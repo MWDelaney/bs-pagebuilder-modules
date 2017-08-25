dc02045c760b664da0a4ee3101517a5efc9f6e77
mwdelaney <michael.delaney@gmail.com>
2017-08-25 12:26:25 -0400

    Call to Action with BS button

--- bscallout/includes/frontend.php
+++ bscallout/includes/frontend.php
@@ -1,16 +1,12 @@
 <div class="<?php echo $module->get_classname(); ?>">
 	<?php
-
 	// Image left
 	$module->render_image('left');
-
 	?>
 	<div class="fl-callout-content">
     <?php
-
     // Image above title
     $module->render_image('above-title');
-
     ?>
     <div class="fl-callout-text-wrap">
       <div class="card">
@@ -18,10 +14,8 @@
 			<?php
 			// Title
 			$module->render_title();
-
 			// Image below title
 			$module->render_image('below-title');
-
 			// Text
 			$module->render_text();
       ?>
@@ -30,17 +24,13 @@
       <?php
 			// Link CTA
 			$module->render_link();
-
 			// Button CTA
 			$module->render_button();
-
 			?>
 		</div>
 	</div>
 	<?php
-
 	// Image right
 	$module->render_image('right');
-
 	?>
 </div>
